5fb863992379ea042e5b079cb6c43e627a34a735
Daniel Medina <daniel@medina.com>
2021-03-06 16:32:20 -0600

    Changed site title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Daniel Medina';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Daniel Medina\'s Blog';
 
 export default function Layout({ children, home }) {
     return (
